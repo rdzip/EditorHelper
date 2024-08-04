16d1d31a76a45edb56bf5f700419b515c85a14a3
Sweet-Swi <channel.iru0000@gmail.com>
2024-08-05 02:31:03 +0900

    fix: korean(and maybe other languages) are not entered if pixel font.

---
+++ EditorHelper/Tweaks/InputFieldKoreanPatch.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx.Configuration;
+using HarmonyLib;
+using RDLevelEditor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace EditorHelper.Tweaks
+{
+	public class InputFieldKoreanPatch : IEditorTweak
+	{
+		private Harmony harmony;
+
+		public void StartTweak(ConfigFile config)
+		{
+			harmony = Harmony.CreateAndPatchAll(typeof(KoreanPatch));
+		}
+
+		public void StopTweak()
+		{
+			harmony.UnpatchSelf();
+		}
+
+		public static class KoreanPatch
+		{
+			[HarmonyPatch(typeof(InspectorPanel_Comment), "LateUpdate")]
+			[HarmonyPostfix]
+			public static void InputOtherLanguage(InspectorPanel_Comment __instance)
+			{
+				RDEditorUtils.UpdateUIText(__instance.dialogue.textComponent, __instance.dialogue.textComponent.text, false);
+			}
+		}
+	}
+}
